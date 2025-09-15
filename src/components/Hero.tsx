c381030517eae081207271b3dbb61f1ff81b9301
OudomPanhaChea <169124803+OudomPanhaChea@users.noreply.github.com>
2025-09-15 18:26:59 +0700

    Implement Hero Scroll Animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,18 +1,24 @@
-import { useGSAP } from "@gsap/react"
+import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { SplitText } from "gsap/all";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
-  useGSAP(() => {
-    const heroSplit = new SplitText('.title', { type: 'chars, words' });
-    const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  // const videoTimelineRef = useRef<gsap.core.Timeline | null>(null)
+  const isMobile = useMediaQuery({ maxWidth: 767 });
 
-    heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
+  useGSAP(() => {
+    const heroSplit = new SplitText(".title", { type: "chars, words" });
+    const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
+
+    heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
 
     gsap.from(heroSplit.chars, {
       yPercent: 100,
       duration: 1.8,
-      ease: 'expo.out',
+      ease: "expo.out",
       stagger: 0.06,
     });
 
@@ -20,28 +26,61 @@ const Hero = () => {
       opacity: 0,
       yPercent: 100,
       duration: 1.8,
-      ease: 'expo.out',
+      ease: "expo.out",
       stagger: 0.06,
-      delay: 0.8
+      delay: 0.8,
     });
 
-    gsap.timeline({
-      scrollTrigger: {
-        trigger: '#hero',
-        start: 'top top',
-        end: 'bottom top',
-        scrub: true,
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .to(".right-leaf", { y: 200 }, 0)
+      .to(".left-leaf", { y: -200 }, 0);
+
+    const startValue = isMobile ? 'top 50%' : 'center 60%';
+    const endValue = isMobile ? '120% top' : 'bottom top';
+
+    
+    if(videoRef.current) {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: 'video',
+          start: startValue,
+          end: endValue,
+          scrub: true,
+          pin: true
+        }
+      });
+
+      videoRef.current.onloadedmetadata = () => {
+        tl.to(videoRef.current, {
+          currentTime: videoRef.current?.duration,
+        })
       }
-    }).to('.right-leaf', { y: 200 }, 0)
-    .to('.left-leaf', { y: -200 }, 0)
-  }, []);
+    }
+  }, [isMobile]);
+
   return (
     <>
       <section id="hero" className="noisy">
         <h1 className="title">MOJITO</h1>
 
-        <img src="/images/hero-left-leaf.png" alt="left-leaf" className="left-leaf"/>
-        <img src="/images/hero-right-leaf.png" alt="right-leaf" className="right-leaf"/>
+        <img
+          src="/images/hero-left-leaf.png"
+          alt="left-leaf"
+          className="left-leaf"
+        />
+        <img
+          src="/images/hero-right-leaf.png"
+          alt="right-leaf"
+          className="right-leaf"
+        />
 
         <div className="body">
           <div className="content">
@@ -54,15 +93,28 @@ const Hero = () => {
 
             <div className="view-cocktails">
               <p className="subtitle">
-                Every cocktails on the menu is a blend of premium ingredients, creative flair, and timeless recipes - designed to delight your senses.
+                Every cocktails on the menu is a blend of premium ingredients,
+                creative flair, and timeless recipes - designed to delight your
+                senses.
               </p>
               <a href="#cocktails">View Cocktails</a>
             </div>
           </div>
         </div>
       </section>
-    </>
-  )
-}
 
-export default Hero
+      <div className="video absolute inset-0">
+        <video 
+          ref={videoRef}
+          src="/videos/output.mp4" 
+          muted 
+          playsInline 
+          preload="auto" 
+          loop
+        />
+      </div>
+    </>
+  );
+};
+
+export default Hero;
